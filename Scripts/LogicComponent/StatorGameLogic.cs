dddada500ac3cb9f61c86f169e01da532c4cc8ec
Sisk S'jet <sisk.sjet@gmail.com>
2024-01-27 23:07:46 +0100

    feat: only allow return to home action when rotor part is attached

--- Scripts/LogicComponent/StatorGameLogic.cs
+++ Scripts/LogicComponent/StatorGameLogic.cs
@@ -24,7 +24,7 @@ namespace Sisk.RotorReturnHome.LogicComponent {
         /// <summary>
         /// Indicates if smart rotor is able to rotate to home position.
         /// </summary>
-        public bool CanReturnToHome => Block.Enabled && !Block.RotorLock;
+        public bool CanReturnToHome => Block.Enabled && !Block.RotorLock && Block.IsAttached;
 
         /// <summary>
         /// Indicates if smart rotor should be disabled when home position is reached.
